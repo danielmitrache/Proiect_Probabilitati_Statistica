f260b23bc748affe6a0387a6c321686daba18056
Daniel Mitrache <mitracheadaniel@gmail.com>
2026-01-19 19:13:56 +0200

    exercitiul 2 complet + documentatie

--- R/02_latenta.R
+++ R/02_latenta.R
@@ -1,27 +1,20 @@
 # Fisier: R/02_latenta.R
 
-# -- Rezolvarea cerintei a) --
+# Rezolvarea cerintei a)
 #' Simuleaza timpii de raspuns (S)
 #' @param n Numarul de cereri (esantionul)
-#' @param tip Tipul distributiei: "exponential" sau "normala"
-#' @param medie_target Timpul mediu dorit (ex: 200 ms)
-simuleaza_latente <- function(n, tip = "exponentiala", medie_target = 200) {
-  
+#' @param tip Tipul distributiei: "exponentiala" sau "normala"
+#' @param medie Timpul mediu dorit (ex: 200 ms) atat pentru distributia exponentiala cat si cea normala
+#' @param sd Deviatia standard pentru distributia normala
+simuleaza_latente <- function(n, tip = "exponentiala", medie = 200, sd = 50) {
   if (tip == "exponentiala") {
-    # 1. Distributia EXPONENTIALA (Asimetrica - Foarte comuna in cozi de asteptare)
-    rate_param <- 1 / medie_target
+    rate_param <- 1 / medie
     valori <- rexp(n, rate = rate_param)
-    
   } else if (tip == "normala") {
-    # 2. Distributia NORMALA (Simetrica - Ideala)
-    # Alegem o deviatie standard proportionala cu media
-    sd_param <- medie_target / 4
-    valori <- rnorm(n, mean = medie_target, sd = sd_param)
+    valori <- rnorm(n, mean = medie, sd = sd)
     
-    # TRUNCHIERE (Valori pozitive):
     # Timpul nu poate fi negativ. Orice valoare < 1ms devine 1ms.
     valori <- pmax(valori, 1)
-    
   } else {
     stop("Tip distributie necunoscut! Alege 'exponentiala' sau 'normala'.")
   }
@@ -30,80 +23,89 @@ simuleaza_latente <- function(n, tip = "exponentiala", medie_target = 200) {
 }
 
 
-# -- Rezolvarea cerintelor b), c) --
-if (sys.nframe() == 0){
-  # Cerinta B
-  # Parametrii simularii
-  nr_cereri <- 10^5   # Presupunem 100.000 de cereri
-  media_target <- 200 # Timpul mediu dorit (ms)
+# Rezolvarea cerintei b)
+#' Vizualizeaza histograma si densitatea teoretica
+#' @param vector_latente Datele generate de functia simuleaza_latente
+#' @param tip "exponentiala" sau "normala"
+#' @param medie Media folosita la generare (pt curba teoretica)
+#' @param sd Deviatia standard (doar pt normala)
+vizualizeaza_latente <- function(vector_latente, tip = "exponentiala", medie = 200, sd = 50) {
+  # Desenam Histograma
+  if (tip == "exponentiala") {
+    titlu <- "Latente: Distribuție Exponențială (Asimetrică)"
+    culoare <- "wheat"
+  } else {
+    titlu <- "Latente: Distribuție Normală (Simetrică)"
+    culoare <- "lightblue"
+  }
   
-  # Generam datele
-  latenta_exp <- simuleaza_latente(nr_cereri, tip="exponentiala", medie_target = media_target)
-  latenta_norm <- simuleaza_latente(nr_cereri, tip="normala", medie_target = media_target)
-  
-  # Setam layout-ul: 1 rand, 2 coloane
-  par(mfrow=c(1, 2))
-  
-  # 1. Distributia EXPONENTIALA
-  hist(latenta_exp, 
+  hist(vector_latente, 
        probability = TRUE, 
-       breaks = 50, 
-       col = "wheat", 
+       breaks = 30, 
+       col = culoare, 
        border = "white",
-       main = "Distributie Exponentiala (Asimetrica)", 
-       xlab = "Timp raspuns (ms)")
+       main = titlu,
+       xlab = "Timp de Răspuns (ms)",
+       ylab = "Densitate")
   
-  # Densitatea teoretica (formula matematica)
-  # Rata (lambda) este 1 / media
-  curve(dexp(x, rate = 1/media_target), 
-        add = TRUE,    # Se pune PESTE graficul existent
-        col = "red",   # Culoare rosie
-        lwd = 2)       # Grosimea liniei
+  # Suprapunem densitatea teoretica (formula matematica)
+
+  if (tip == "exponentiala") {
+    # Functia de densitate exponentiala este dexp
+    curve(dexp(x, rate = 1/medie), 
+          col = "red", lwd = 3, add = TRUE)
+  } else {
+    # Functia de densitate normala este dnorm
+    curve(dnorm(x, mean = medie, sd = sd), 
+          col = "darkblue", lwd = 3, add = TRUE)
+  }
+}
+
+
+
+# Rezolvarea cerintei c)
+#' Calculeaza statisticile descriptive (Media, Varianta, Mediana, Modul)
+#' si returneaza un tabel comparativ.
+#'
+#' @param v_exp Vectorul cu datele distributiei Exponentiale
+#' @param v_norm Vectorul cu datele distributiei Normale
+#' @return Un Data Frame cu statisticile rotunjite la 2 zecimale
+comparatie_statistica_latente <- function(v_exp, v_norm) {
   
-  # 2. Distributia NORMALA
-  hist(latenta_norm, 
-       probability = TRUE, 
-       breaks = 50, 
-       col = "lightblue", 
-       border = "white",
-       main = "Distributie Normala (Simetrica)", 
-       xlab = "Timp raspuns (ms)")
-  
-  # Densitatea teoretica
-  # SD-ul folosit in functie este media / 4
-  curve(dnorm(x, mean = media_target, sd = media_target/4), 
-        add = TRUE, 
-        col = "blue", 
-        lwd = 2)
-  
-  # Resetam layout-ul
-  par(mfrow=c(1, 1))
-  
-  
-  # Cerinta C
+  # Pentru variabile continue, Modul este varful curbei de densitate
   calculeaza_modul <- function(v) {
     d <- density(v)
     return(d$x[which.max(d$y)])
   }
-  # --- 1. Statistici pentru EXPONENTIALA ---
-  mean_exp <- mean(latenta_exp)
-  var_exp  <- var(latenta_exp)
-  med_exp  <- median(latenta_exp)
-  mod_exp  <- calculeaza_modul(latenta_exp)
   
-  # --- 2. Statistici pentru NORMALA ---
-  mean_norm <- mean(latenta_norm)
-  var_norm  <- var(latenta_norm)
-  med_norm  <- median(latenta_norm)
-  mod_norm  <- calculeaza_modul(latenta_norm)
-  
-  df_stat <- data.frame(
-    Distributia = c("Exponentiala", "Normala"),
-    Media    = c(mean_exp, mean_norm),
-    Varianta = c(var_exp, var_norm),
-    Mediana  = c(med_exp, med_norm),
-    Modul    = c(mod_exp, mod_norm)
+  # Calculam statisticile pentru exponentiala
+  stats_exp <- c(
+    Media    = mean(v_exp),
+    Varianta = var(v_exp),
+    Mediana  = median(v_exp),
+    Modul    = calculeaza_modul(v_exp)
   )
   
-  print(df_stat)
+  # Calculam statisticile pentru normala
+  stats_norm <- c(
+    Media    = mean(v_norm),
+    Varianta = var(v_norm),
+    Mediana  = median(v_norm),
+    Modul    = calculeaza_modul(v_norm)
+  )
+  
+  # Construim Data Frame-ul final
+  df_stat <- data.frame(
+    Distributia = c("Exponentiala", "Normala"),
+    Media    = c(stats_exp["Media"],    stats_norm["Media"]),
+    Varianta = c(stats_exp["Varianta"], stats_norm["Varianta"]),
+    Mediana  = c(stats_exp["Mediana"],  stats_norm["Mediana"]),
+    Modul    = c(stats_exp["Modul"],    stats_norm["Modul"])
+  )
+  
+  # Rotunjim valorile numerice pentru un aspect curat in tabel
+  # (Coloanele 2 pana la 5 sunt numerice)
+  df_stat[, 2:5] <- round(df_stat[, 2:5], 2)
+  
+  return(df_stat)
 }
